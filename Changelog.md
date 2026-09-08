646563777407571e56513ef198dbff41a539640c
cainmagi <cainmagi@gmail.com>
2026-09-08 08:37:39 -0500

    1.0.0 - fix social card link Fix: Correct a bug of the social card link.

--- Changelog.md
+++ Changelog.md
@@ -10,3 +10,7 @@
 
 1. Create this project.
 2. Upload the first version of the document, containing the tutorial and API docs.
+
+#### :wrench: Fix
+
+1. Fix: Correct a bug of the social card link.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,7 +79,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/social-card.jpg",
+    image: "img/social-card.webp",
     colorMode: {
       respectPrefersColorScheme: true,
     },
